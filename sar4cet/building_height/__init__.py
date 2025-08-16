1afd6993d62790e8106bc667576c35a5ea80b820
Parth Rajubhai Naik <naik15@fwu076.ad.fz-rossendorf.de>
2025-08-16 16:58:50 +0200

    update

--- sar4cet/building_height/__init__.py
+++ sar4cet/building_height/__init__.py
@@ -1,12 +1,11 @@
 # Building Height Estimation Module
 
-from .height_estimation import estimate_building_heights, extract_building_features
+from .height_estimation import estimate_building_heights
 from .regression_networks import BoundingBoxRegressor, train_height_model
 from .visualization import plot_height_map, visualize_building_detections
 
 __all__ = [
     'estimate_building_heights',
-    'extract_building_features',
     'BoundingBoxRegressor',
     'train_height_model',
     'plot_height_map',
